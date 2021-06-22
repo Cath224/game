bb123398d3fa2f91434ed41b0c0e5a054c225b83
mvpeskova <85232764+mvpeskova@users.noreply.github.com>
2021-06-22 23:58:20 +0300

    structures

---
+++ game1.cpp
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 56
+
+struct roslina
+{
+    char nazwa[20];
+    int czas;
+    int cena_kup;
+    int cena_sprzedaj;
+};
+struct dzien
+{
+    int pogoda;
+    short targ;
+    short sklep;
+    int los1;
+    int los2;
+};
+
+
+struct plon
+{
+    int szt;
+    int nowe;
+};
+
+struct blok
+{
+    int id;
+    int stan;
+    int dni;
+};
+
+int main()
+{
+
+}
